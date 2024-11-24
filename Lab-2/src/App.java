e2f946aa2c83695e6b0b607aa954f72b880ff386
BadrGello <138599821+BadrGello@users.noreply.github.com>
2024-11-24 22:03:51 +0200

    Accident Removal

--- Lab-2/src/App.java
+++ Lab-2/src/App.java
@@ -1,6 +1,19 @@
 import java.util.Map;
 
 public class App {
+    
+    public static int computeGCD(int a ,int b){
+        if(b==0) return a;
+        else{
+        return computeGCD(b, a%b);
+        }
+     }
+     public static int computeLCM(int a ,int b){
+         int gcd=computeGCD(a, b);
+         int lcm=gcd*(a/gcd)*(b/gcd);
+         return lcm;
+      }
+ 
     public static void main(String[] args) throws Exception {
 
         int n = 7561;
