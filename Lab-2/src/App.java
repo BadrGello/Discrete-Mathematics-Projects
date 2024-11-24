856f864d60c4fbb6d784cd098a37eef677193646
adham165 <adhamanas2004@gmail.com>
2024-11-24 21:09:39 +0200

    GCDandLCM euclidean

--- Lab-2/src/App.java
+++ Lab-2/src/App.java
@@ -1,4 +1,16 @@
 public class App {
+    public static int computeGCD(int a ,int b){
+        if(b==0) return a;
+        else{
+        return computeGCD(b, a%b);
+        }
+     }
+     public static int computeLCM(int a ,int b){
+         int gcd=computeGCD(a, b);
+         int lcm=gcd*(a/gcd)*(b/gcd);
+         return lcm;
+      }
+ 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
